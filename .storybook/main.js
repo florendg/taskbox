4c7cb2f97dd73c70e95784b0cf213d8b449440d9
floren <florendg@gmail.com>
2022-12-25 09:11:13 +0100

    Upgrade to latest beta version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,14 @@
 module.exports = {
   stories: ["../src/app/**/*.stories.ts"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
-  framework: "@storybook/angular",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
+  framework: {
+    name: "@storybook/angular",
+    options: {}
   },
   features: {
-    interactionsDebugger: true,
+    interactionsDebugger: true
   },
+  docs: {
+    docsPage: "automatic"
+  }
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './task/task.component';
-import { TaskListComponent } from './task-list/task-list.component';
 import { StoreModule } from '@ngrx/store';
+import { TaskListComponent } from './task-list/task-list.component'
 
 @NgModule({
   declarations: [AppComponent, TaskComponent, TaskListComponent],
